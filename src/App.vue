2293e852aa0c29a35d852430a41e4b5532b58342
Adelina-Chiriac <adelinachiriac@gmail.com>
2022-12-04 10:57:33 +0100

    add axios library & create VideoList component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
     <SearchBar @searchTermChange="onSearchTermChange"></SearchBar>
+    <VideoList></VideoList>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+
+const apiKey = process.env.VUE_APP_API_KEY;
 
 export default {
   name: "App",
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
   },
   methods: {
     onSearchTermChange: function(searchTerm) {
-      console.log(searchTerm);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: apiKey,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => console.log(response));
     }
   }
 };
