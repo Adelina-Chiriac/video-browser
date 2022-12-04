73ba2b2e7fd529ce57db7786e4ac50669bad4ece
Adelina-Chiriac <adelinachiriac@gmail.com>
2022-12-04 20:08:34 +0100

    add embedded video

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,8 @@
 <template>
   <div v-if="video">
+    <div>
+      <iframe :src="videoUrl" />
+    </div>
     <div class="details">
       <h4>{{ video.snippet.title }}</h4>
       <p>{{ video.snippet.description }}</p>
@@ -12,9 +15,22 @@ export default {
   name: "VideoDetail",
   props: {
     video: Object
+  },
+  computed: {
+    videoUrl: function() {
+      const videoId = this.video.id.videoId;
+      
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
+  .details {
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
 </style>
